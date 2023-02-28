7ce671a9f357568c6daa2de8113476fa9843bd70
JuliaShi <52406982+JuliyaShi@users.noreply.github.com>
2023-02-28 16:54:06 +0100

    O(n)
    var twoSum = function(nums, target) {
        let map = new Map();
        for(i = 0; i<nums.length; i ++){
            let num1 = nums[i];
            let num2 = target - num1;
            if(map.has(num2)){
                return [i, map.get(num2)]
            }
            map.set(num1, i)
        }
    };

--- src/two-sum.js
+++ src/two-sum.js
@@ -1,3 +1,4 @@
+O(n^2)
 const twoSum = (a, num) => {
   for (let i = 0; i < a.length - 1; i += 1) {
     for (let j = i + 1; j < a.length; j += 1) {
@@ -18,4 +19,17 @@ console.log(twoSum(a, 5));
 console.log(twoSum(a, 6));
 console.log(twoSum(a, 7));
 console.log(twoSum(a, 8));
+
+O(n)
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for(i = 0; i<nums.length; i ++){
+        let num1 = nums[i];
+        let num2 = target - num1;
+        if(map.has(num2)){
+            return [i, map.get(num2)]
+        }
+        map.set(num1, i)
+    }
+};
 
